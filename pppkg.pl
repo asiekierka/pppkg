bd202bfff9c935bb091150d7085790b7b2a45bd5
Adrian Siekierka <asiekierka@gmail.com>
2012-04-08 02:49:09 +0200

    partial error checking, partial uninstallation. usable enough for a proof of concept.

--- pppkg.pl
+++ pppkg.pl
@@ -3,8 +3,8 @@
 # Builtin
 use File::Temp qw(tempfile tempdir);
 use Archive::Tar;
-use File::Path qw(make_path remove_tree);
 use File::Copy;
+use File::Path qw(rmtree);
 # Additional
 use JSON;
 use IO::Uncompress::Bunzip2 qw(bunzip2 $Bunzip2Error);
@@ -22,48 +22,97 @@ sub die_error {
 }
 
 sub cmd_help {
-	print "\n[ Portable Perl PacKaGist 0.1 ]\n";
+	print "\n[ Portable(-ish) Perl PacKaGist 0.1 ]\n";
 	print "Options:\n";
 	print "\t-h\t\tHelp\n\t-i [pkg" . $package_ext . "]\tInstall package file\n";
+	print "\t-r [pkg-name]\tRemove package (UNFINISHED)\n";
 	print "\t-v\t\tVerbose\n\t-f\t\tForce\n\t\-P [prefix]\tPrefix folder\n";
 	print "\n";
 }
 
 sub hardlink_copy {
-	my ($src, $dest, $verbose) = @_;
+	my ($src, $dest, $verbose, $force) = @_;
 	# Screw the Perl ways! Bash on it!
-	open(FILELIST, ">filelist") || die_error("Cannot create filelist!",8);
+	open(FILELIST, ">","filelist")
+		or die_error("Cannot create filelist!",8);
 	chdir $src;
 	my @files = `find *`;
 	foreach $file (@files) {
 		print FILELIST $file;
 		$file=~s/\n//g;
 		if($verbose>=2) { print $dest.$file . "\n"; }
-		system("ln ".$src.$file." ".$dest.$file);
+		$errcode = system("ln ".$src.$file." ".$dest.$file);
+		unless($errcode==0) { if($force<1) { print "[WARNING] Error while hardlinking " . $file . "!\n"; } }
 	}
 	close(FILELIST);
+}
+sub readJSON {
+	my $filename = shift;
+	open(FILE,"<",$filename)
+		or die_error("Could not read JSON file!",5);
+	my $fh_text = "";
+	foreach $line (<FILE>) { $fh_text .= $line; };
+	close(FILE);
+	return decode_json($fh_text);
+}
+sub cmd_uninstall {
+	my ($package, $verbose, $force) = @_;
+	my $pkgdir = $prefix."var/pkg/".$package;
+	# Abuse the fact that the filelist is generated during the hardlinking.
+	unless(-e $pkgdir."/filelist"){ die_error("Package " . $package . " isn't installed!"); }
+	print "Removing package " . $package . "...\n";
+	if($verbose>=2) { print "Reading filelist...\n"; }
+	open(FILELIST, "<", $pkgdir."/filelist")
+		or die_error("Cannot open filelist!",5);
+	my @files = <FILELIST>;
+	close(FILELIST);
+	my $fullpath = "";
+	my @dirs = ();
+	foreach $file (@files) {
+		$file=~s/\n//g;
+		$fullpath = $prefix.$file;
+		if(-e $fullpath) {
+			if($verbose>=2) { print "Removing " . $fullpath . "\n"; }
+			if(-f $fullpath) {
+				unlink($fullpath)
+					or die_error("COULDN'T UNLINK FILE! THIS MAY MEAN A BROKEN SYSTEM",9);
+			} elsif(-d $fullpath) {
+				push(@dirs,$fullpath);
+			}
+		}
+	}
+	# We have to check for empty dirs. You know.
+	foreach $dir (@dirs) {
+		unless (scalar <$dir/*>) {
+			if($verbose>=2) { print "Removing " . $dir . "\n"; }
+			rmdir($dir);
+		}
+	}
+	if($verbose>=1) { print "Removing package...\n"; }
+	rmtree($pkgdir)
+		or die_error("Couldn't remove package!",10);
+	print "Package ".$package." uninstalled successfully!\n";
 }
 sub cmd_install {
 	my ($package, $verbose, $force) = @_;
 	print "Installing package " . $package . "...\n";
-	unless(-e $package) { die_error("File doesn't exist",3); }
+	unless(-e $package) { $package=$package.".ppk";
+		unless(-e $package) { die_error("File doesn't exist",3); }
+	}
 	if($verbose>=2) { print "Creating tempdir...\n"; }
 	$tempdir = tempdir("/tmp/pkgist-XXXXXXXX", CLEANUP => 0);	
 	if($verbose>=1) { print "Unpacking package...\n"; }
 	$temparch = $tempdir . "/pkg.tar";
-	bunzip2 $package => $temparch
-		or die_error("[BZIP2] ".$Bunzip2Error,4);
+	bunzip2 $package => $temparch or die_error("[BZIP2] ".$Bunzip2Error,4);
 	chdir $tempdir;
-	Archive::Tar->extract_archive("pkg.tar")
-		or die_error("[TAR]",4);
+	Archive::Tar->extract_archive("pkg.tar") or die_error("[TAR] An error!",4);
+	unlink("pkg.tar");
 	print "Reading package...\n";
-	open(FILE,"<","info.json")
-		or die_error("Could not read package!",5);
-	my $fh_text = "";
-	foreach $line (<FILE>) { $fh_text .= $line; };
-	close(FILE);
-	my $package_info = decode_json($fh_text);
-	my $pkgname = $package_info->{meta}->{name} . "-" . $package_info->{meta}->{version};
+	my $package_info = readJSON("info.json");
+	my $pkgname = $package_info->{meta}->{name};
+	my $pkgdir = $prefix."var/pkg/".$pkgname;
+	my $rootdir = $pkgdir."/root";
+	if(-d $rootdir){ die_error("The package is already installed! Uninstall it first.",8); }
 	unless(-d "root")
 	{
 		print "Compiling...\n";
@@ -74,13 +123,14 @@ sub cmd_install {
 	if($verbose>=1) { print "Moving package...\n"; }
 	unless(-d ($prefix . "var")){mkdir ($prefix."var");}
 	unless(-d ($prefix . "var/pkg")){mkdir ($prefix."var/pkg");}
-	unless(-d ($prefix . "var/pkg/".$pkgname)){mkdir ($prefix."var/pkg/".$pkgname);}
+	unless(-d ($pkgdir)){mkdir ($pkgdir);}
 	# Should be more portable, really. But who'll want to install this on Windows?
-	system("mv ".$tempdir."/* ".$prefix."var/pkg/".$pkgname);
-	my $rootdir = $prefix . "var/pkg/".$pkgname."/root";
+	# Is that even possible?
+	system("mv ".$tempdir."/* ".$pkgdir);
 	unless(-d $rootdir){ die_error("Moving failed: $?",7); }
 	print "Installing files...\n";
-	hardlink_copy($rootdir."/", $prefix, $verbose);
+	chdir $pkgdir;
+	hardlink_copy($rootdir."/", $prefix, $verbose, $force);
 	print "Package " . $pkgname . " installed!\n";
 	exit(0);
 }
@@ -104,6 +154,12 @@ for(;$args<$argv_len;$args++)
 			$package = $ARGV[$args+1];
 			$args++;
 		} else { die_error("Package not specified.",2); }
+	} elsif($ARGV[$args] eq "-r") {
+		if($args<($argv_len-1)) {
+			$command = "remove";
+			$package = $ARGV[$args+1];
+			$args++;
+		} else { die_error("Package not specified.",2); }
 	} elsif($ARGV[$args] eq "-P") {
 		if($args<($argv_len-1)) {
 			$prefix = $ARGV[$args+1];
@@ -119,5 +175,6 @@ for(;$args<$argv_len;$args++)
 	}
 }
 
-if($command=="install") { cmd_install($package,$verbose,$force); }
-else { die_error("No command specified.\nUse -h for help.\n"); }
+if($command eq "install") { cmd_install($package,$verbose,$force); }
+elsif($command eq "remove") { cmd_uninstall($package,$verbose,$force); }
+else { die_error("No command specified.\nUse -h for help."); }
